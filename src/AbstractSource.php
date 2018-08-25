6d427a2a0418112027fd24a7fb1e24f3c163a620
anders@berle.cc <anders@berle.cc>
2018-08-25 18:35:32 +0000

    Implement pageQuery default

--- src/AbstractSource.php
+++ src/AbstractSource.php
@@ -11,12 +11,19 @@ abstract class AbstractSource implements SourceInterface
     abstract public function store(array $resources): void;
     abstract public function remove(array $resources): void;
     
-    abstract public function pageQuery(QueryInterface $query, int $page, int $size): CollectionInterface
     abstract public function allQuery(QueryInterface $query): CollectionInterface;
+    abstract public function getQuery(QueryInterface $query, $id);
 
     public function dependencies(): array
     {
         return [];
+    }
+    
+    public function pageQuery(QueryInterface $query, int $page, $size): CollectionInterface
+    {
+        $items = $this->allQuery($query);
+        
+        return array_slice($items, $size * ($page - 1), $size);
     }
 
     public function eachQuery(QueryInterface $query, \Closure $callback): void
@@ -33,5 +40,5 @@ abstract class AbstractSource implements SourceInterface
     {
         return new Collection($data);
     }
-
+    
 }
